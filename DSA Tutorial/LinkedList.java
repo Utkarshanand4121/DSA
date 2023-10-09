9211798753b2c8d50be9277849daefd6303c4784
Utkarsh Anand <utkarshanand4121@gmail.com>
2023-10-09 11:10:54 +0530

    Zig Zag

--- DSA Tutorial/LinkedList.java
+++ DSA Tutorial/LinkedList.java
@@ -325,6 +325,45 @@ public class LinkedList {
         // merge
         return merge(newLeft, newRight);
     }
+
+    public void zigZag() {
+        // mid
+        Node slow = head;
+        Node fast = head.next;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node mid = slow;
+
+        // reverse 2nd half
+        Node curr = mid.next;
+        mid.next = null;
+        Node prev = null;
+        Node next;
+
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        Node left = head;
+        Node right = prev;
+        Node nextL, nextR;
+
+        // alt merge - zig-zag merge
+        while(left != null && right != null) {
+            nextL = left.next;
+            left.next = right;
+            nextR = right.next;
+            right.next = nextL;
+
+            left = nextL;
+            right = nextR;
+        }
+    }
     public static void main(String[] args) {
         // LinkedList l1 = new LinkedList();
         // l1.addFirst(2);
@@ -353,15 +392,13 @@ public class LinkedList {
         // System.out.println(isCycle());
 
         LinkedList ll = new LinkedList();
-        ll.addFirst(1);
-        ll.addFirst(2);
-        ll.addFirst(3);
-        ll.addFirst(4);
-        ll.addFirst(5);
-        // 5->4->3->2->1
-
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.addLast(5);
         ll.print();
-        ll.head = ll.mergeSort(ll.head);
+        ll.zigZag();
         ll.print();
     }
 }

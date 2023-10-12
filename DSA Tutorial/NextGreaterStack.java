9aaeacba208b6f3787b09cd7bd2bda70e034acde
Utkarsh Anand <utkarshanand4121@gmail.com>
2023-10-12 19:04:34 +0530

    Next Greater Element Imp

--- DSA Tutorial/NextGreaterStack.java
+++ DSA Tutorial/NextGreaterStack.java
@@ -7,19 +7,19 @@ public class NextGreaterStack {
         int nxtGreater[] = new int[arr.length];
 
         for (int i = arr.length - 1; i >= 0; i--) {
-            // 1 While
+            // 1 - While
             while (!s.isEmpty() && arr[s.peek()] <= arr[i]) {
                 s.pop();
             }
 
-            // 2 if-else
+            // 2 - if-else
             if (s.isEmpty()) {
                 nxtGreater[i] = -1;
             } else {
                 nxtGreater[i] = arr[s.peek()];
             }
 
-            // 3 push in s
+            // 3 - push in s
             s.push(i);
         }
         for (int i = 0; i < nxtGreater.length; i++) {

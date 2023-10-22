05b62084476be2c144fe2011a0de473316e12fb8
Utkarsh Anand <utkarshanand4121@gmail.com>
2023-10-22 17:29:24 +0530

    Binary Tree Tutorial

--- DSA Tutorial/BinaryTrees.java
+++ DSA Tutorial/BinaryTrees.java
@@ -239,6 +239,84 @@ public class BinaryTrees {
         System.out.println();
     }
 
+    // Kth level of tree
+    public static void KLevel(Node root, int level, int k) {
+        if(root == null) {
+            return;
+        }
+
+        if(level == k) {
+            System.out.print(root.data+" ");
+            return;
+        }
+
+        KLevel(root.left, level+1, k);
+        KLevel(root.right, level+1, k);
+    }
+
+    // Lowest common Ancestor
+    public static boolean getPath(Node root, int n, ArrayList<Node> path) {
+
+        if(root == null) {
+            return false;
+        }
+        path.add(root);
+        if(root.data == n) {
+            return true;
+        } 
+
+        boolean foundLeft = getPath(root.left, n, path);
+        boolean foundRight = getPath(root.right, n, path);
+
+        if(foundLeft || foundRight) {
+            return true;
+        }
+
+        path.remove(path.size() - 1);
+        return false;
+    }
+    public static Node lca(Node root, int n1, int n2) { // O(n)
+        ArrayList<Node> path1 = new ArrayList<>();
+        ArrayList<Node> path2 = new ArrayList<>();
+
+        getPath(root, n1, path1);
+        getPath(root, n2, path2);
+
+        // last common ancestor
+        int i=0;
+        for(; i<path1.size()&& i<path2.size(); i++) {
+            if(path1.get(i) != path2.get(i)) {
+                break;
+            }
+        }
+
+        // last equal node -> i-1th
+        Node lca = path1.get(i-1);
+        return lca;
+    }
+
+    public static Node lca2(Node root, int n1, int n2) {
+
+        if(root == null) {
+            return null;
+        }
+        if(root.data == n1 || root.data == n2) {
+            return root;
+        }
+
+        Node leftLca = lca2(root.left, n1, n2);
+        Node rightLca = lca2(root.right, n1, n2);
+
+        // leftLCA = val rightLca = null
+        if(rightLca == null) {
+            return leftLca;
+        }
+        if(leftLca == null) {
+            return rightLca;
+        }
+
+        return root;
+    }
     public static void main(String[] args) {
         // int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         // BinaryTree tree = new BinaryTree();
@@ -280,6 +358,12 @@ public class BinaryTrees {
         // subRoot.right = new Node(5);
 
         // System.out.println(isSubtree(root, subRoot));
-        topView(root);
+        // topView(root);
+
+        // int k = 3;
+        // KLevel(root, 1, k);
+
+        int n1 = 4, n2 = 6;
+        System.out.println(lca2(root, n1, n2).data);
     }
 }

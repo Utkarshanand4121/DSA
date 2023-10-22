084875317ab43c73dce96c5061a65719cba9059b
Utkarsh Anand <utkarshanand4121@gmail.com>
2023-10-22 19:44:13 +0530

    Kth ancestor of node

--- DSA Tutorial/BinaryTrees.java
+++ DSA Tutorial/BinaryTrees.java
@@ -317,6 +317,58 @@ public class BinaryTrees {
 
         return root;
     }
+
+    // Min distance between nodes
+
+    public static int lcaDist(Node root, int n) {
+        if(root == null) {
+            return -1;
+        }
+
+        if(root.data == n) {
+            return 0;
+        }
+
+        int leftDist = lcaDist(root.left, n);
+        int rightDist = lcaDist(root.right, n);
+        if(leftDist == -1 && rightDist == -1) {
+            return -1;
+        } else if(leftDist == -1) {
+            return rightDist + 1;
+        } else {
+            return leftDist + 1;
+        }
+    }
+    public static int minDis(Node root,int n1, int n2) {
+        Node lca = lca2(root, n1, n2);
+        int dist1 = lcaDist(lca, n1);
+        int dist2 = lcaDist(lca, n2);
+
+        return dist1 + dist2;
+    }
+
+    // Kth ancestor of node
+    public static int KAncestor(Node root, int n, int k) {
+        if(root == null) {
+            return -1;
+        }
+        if(root.data == n) {
+            return 0;
+        }
+
+        int leftDist = KAncestor(root.left, n, k);
+        int rightDist = KAncestor(root.right, n, k);
+
+        if(leftDist == -1 && rightDist == -1) {
+            return -1;
+        }
+
+        int max = Math.max(leftDist, rightDist);
+        if(max+1 == k) {
+            System.out.println(root.data);
+        }
+        return max + 1;
+    }
     public static void main(String[] args) {
         // int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         // BinaryTree tree = new BinaryTree();
@@ -363,7 +415,8 @@ public class BinaryTrees {
         // int k = 3;
         // KLevel(root, 1, k);
 
-        int n1 = 4, n2 = 6;
-        System.out.println(lca2(root, n1, n2).data);
+        int n1 = 4, k = 2;
+        //System.out.println(minDis(root, n1, n2));
+        KAncestor(root, n1, k);
     }
 }

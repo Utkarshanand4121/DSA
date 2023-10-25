32a761884b6b7b80613c150f5548b7d7a28b37f4
Utkarsh Anand <utkarshanand4121@gmail.com>
2023-10-25 18:14:53 +0530

    BST Tutorial

--- DSA Tutorial/BST.java
+++ DSA Tutorial/BST.java
@@ -266,6 +266,53 @@ public class BST {
 
         return new Info(false, size, min, max);
     }
+
+    // Merge Two BST
+    public static Node createBST2(ArrayList<Integer> arr, int st, int end) {
+        if(st > end) {
+            return null;
+        }
+        int mid = (st + end) / 2;
+        Node root = new Node(arr.get(mid));
+        root.left = createBST(arr, st, mid-1);
+        root.right = createBST(arr, mid+1, end);
+        return root;
+    }
+    public static Node mergeBSTs(Node root1, Node root2) { // O(n+m) -> linear
+        // step1
+        ArrayList<Integer> arr1 = new ArrayList<>();
+        getInorder(root1, arr1);
+
+        // step 2
+        ArrayList<Integer> arr2 = new ArrayList<>();
+        getInorder(root2, arr2);
+
+        // merge
+        int i=0, j=0;
+        ArrayList<Integer> finalArr = new ArrayList<>();
+        while(i<arr1.size() && j<arr2.size()) {
+            if(arr1.get(i) <= arr2.get(j)) {
+                finalArr.add(arr1.get(i));
+                i++;
+            } else {
+                finalArr.add(arr2.get(j));
+                j++;
+            }
+        }
+
+        while(i<arr1.size()) {
+            finalArr.add(arr1.get(i));
+            i++;
+        }
+
+        while(j<arr2.size()) {
+            finalArr.add(arr2.get(j));
+            j++;
+        }
+
+        // sorted Al - Balanced BST
+        return createBST2(finalArr, 0, finalArr.size()-1);
+    }
     public static void main(String[] args) {
         // int values[] = { 8, 5, 3, 1, 4, 6, 10, 11, 14 };
         // Node root = null;
@@ -325,18 +372,29 @@ public class BST {
         // root = balancedBST(root);
         // preOrder(root);
 
-        Node root = new Node(50);
-        root.left = new Node(30);
-        root.left.left = new Node(5);
-        root.left.right = new Node(20);
+        // Node root = new Node(50);
+        // root.left = new Node(30);
+        // root.left.left = new Node(5);
+        // root.left.right = new Node(20);
 
-        root.right = new Node(60);
-        root.right.left = new Node(45);
-        root.right.right = new Node(70);
-        root.right.right.left = new Node(65);
-        root.right.right.right = new Node(80);
+        // root.right = new Node(60);
+        // root.right.left = new Node(45);
+        // root.right.right = new Node(70);
+        // root.right.right.left = new Node(65);
+        // root.right.right.right = new Node(80);
 
-        Info info = largestBST(root);
-        System.out.println("largest BST size = "+ maxBST);
+        // Info info = largestBST(root);
+        // System.out.println("largest BST size = "+ maxBST);
+
+        Node root1 = new Node(2);
+        root1.left = new Node(1);
+        root1.right = new Node(4);
+
+        Node root2 = new Node(9);
+        root2.left = new Node(3);
+        root2.right = new Node(12);
+
+        Node root = mergeBSTs(root1, root2);
+        preOrder(root);
     }
 }

6dd606e86bd3b1dccdc4013d896d55d13cada947
Utkarsh Anand <utkarshanand4121@gmail.com>
2023-11-02 18:48:40 +0530

    Hashmap Tut

--- DSA Tutorial/Hashmapp.java
+++ DSA Tutorial/Hashmapp.java
@@ -49,6 +49,85 @@ public class Hashmapp {
 
         return map.isEmpty();
     }
+
+    // Count Distinct Elements
+    public static void count() {
+        int num[] = {4,3,2,5,6,7,3,4,2,1};
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i=0; i<num.length; i++) {
+            set.add(num[i]);
+        }
+
+        System.out.println("ans = "+set.size());
+    }
+
+    // Union and intersection
+    public static void unionInter() {
+        int arr1[] = {7,3,9};
+        int arr2[] = {6,3,9,2,9,4};
+        HashSet<Integer> set = new HashSet<>();
+
+        // union
+        for(int i=0; i<arr1.length; i++) {
+            set.add(arr1[i]);
+        }
+
+        for(int i=0; i<arr2.length; i++) {
+            set.add(arr2[i]);
+        }
+
+        System.out.println("union = " + set.size());
+
+        // intersection
+        set.clear();
+        for(int i=0; i<arr1.length; i++) {
+            set.add(arr1[i]);
+        }
+
+        int count = 0;
+        for(int i=0; i<arr2.length; i++) {
+            if(set.contains(arr2[i])) {
+                count++;
+                set.remove(arr2[i]);
+            }
+        }
+
+        System.out.println("intersection = "+ count);
+    }
+
+    // Find itinerary for tickets
+    public static String getStart(HashMap<String, String> tickets) {
+        HashMap<String, String> revMap = new HashMap<>();
+
+        for(String key : tickets.keySet()) {
+            revMap.put(tickets.get(key), key);
+        }
+
+        for(String key : tickets.keySet()) {
+            if(!revMap.containsKey(key)) {
+                return key; // starting point
+            }
+        }
+
+        return null;
+    }
+    public static void iterTicket() { // O(n)
+        HashMap<String, String> tickets = new HashMap<>();
+        tickets.put("Chennai", "Bengaluru");
+        tickets.put("Mumbai", "Delhi");
+        tickets.put("Goa", "Chennai");
+        tickets.put("Delhi", "Goa");
+
+        String start = getStart(tickets);
+        System.out.print(start);
+        for(String key : tickets.keySet()) {
+            System.out.print(" -> " + tickets.get(start));
+            start = tickets.get(start);
+        }
+
+        System.out.println();
+    }
     public static void main(String[] args) {
         // Create
         // HashMap<String, Integer> hm = new HashMap<>();
@@ -104,23 +183,58 @@ public class Hashmapp {
 
         // System.out.println(isAnangram(s, t));
 
-        HashSet<Integer> set = new HashSet<>();
+        // HashSet<Integer> set = new HashSet<>();
 
-        set.add(1);
-        set.add(2);
-        set.add(4);
-        set.add(2);
-        set.add(1);
+        // set.add(1);
+        // set.add(2);
+        // set.add(4);
+        // set.add(2);
+        // set.add(1);
 
-        System.out.println(set);
-        set.clear();
-        System.out.println(set.size());
-        System.out.println(set.isEmpty());
+        // // Iterate in a set
+        // System.out.println(set);
+        // Iterator it = set.iterator();
+        // while(it.hasNext()) {
+        //     System.out.println(it.next());
+        // }
+
+        // for(Integer net : set) {
+        //     System.out.println(net);
+        // }
+        // set.clear();
+        // System.out.println(set.size());
+        // System.out.println(set.isEmpty());
         // set.remove(2);
         // if(set.contains(2)) {
         //     System.out.println("set contains 2");
         // } 
         
+        // HashSet<String> cities = new HashSet<>();
+        // cities.add("Delhi");
+        // cities.add("Mumbai");
+        // cities.add("Noida");
+        // cities.add("Pune");
+        // System.out.println(cities);
 
+        // LinkedHashSet<String> lhs = new LinkedHashSet<>();
+        // lhs.add("Delhi");
+        // lhs.add("Mumbai");
+        // lhs.add("Noida");
+        // lhs.add("Pune");
+        // lhs.add("Bangaluru");
+        // System.out.println(lhs);
+
+        // TreeSet<String> ts = new TreeSet<>();
+        // ts.add("Delhi");
+        // ts.add("Mumbai");
+        // ts.add("Noida");
+        // ts.add("Pune");
+        // ts.add("Bangaluru");
+        // System.out.println(ts);
+
+        // count();
+
+        //unionInter();
+        iterTicket();
     }
 }

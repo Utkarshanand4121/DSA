6a494a2923d286e0cbcef6cbc21020c3405c5551
Utkarsh Anand <utkarshanand4121@gmail.com>
2023-11-03 20:04:24 +0530

    Tries Tutorial

--- DSA Tutorial/Triess.java
+++ DSA Tutorial/Triess.java
@@ -3,7 +3,7 @@ public class Triess {
         Node children[] = new Node[26];
         boolean eow = false;
 
-        Node() {
+        public Node() {
             for(int i=0; i<26; i++) {
                 children[i] = null;
             }
@@ -11,7 +11,54 @@ public class Triess {
     }
 
     public static Node root = new Node();
+
+    public static void insert(String word) { // O(L)
+        Node curr = root;
+        for(int level = 0; level<word.length(); level++) {
+            int idx = word.charAt(level) - 'a';
+            if(curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+        }
+
+        curr.eow = true;
+    }
+
+    // search
+    public static boolean search(String key) { // O(L)
+        Node curr = root;
+        for(int level=0; level<key.length(); level++) {
+            int idx = key.charAt(level) - 'a';
+            if(curr.children[idx] == null) {
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+        return curr.eow == true;
+    }
+
+    public static boolean wordBreak(String key) { // O(L)
+        if(key.length() == 0) {
+            return true;
+        }
+        for(int i=1; i<=key.length(); i++) {
+            if(search(key.substring(0, i)) && wordBreak(key.substring(i))) {
+                return true;
+            }
+        }
+
+        return false;
+    }
     public static void main(String[] args) {
-        
+        String words[] = {"i", "like", "sam", "samsung", "mobile", "ice"};
+        for(int i=0; i<words.length; i++) {
+            insert(words[i]);
+        }
+
+        String key = "ilikesamsung";
+        System.out.println(wordBreak(key));
+        // System.out.println(search("thee"));
+        // System.out.println(search("thor"));
     }
 }

07201cd03c49eee537f1b88d94a9073c2e5e952e
Utkarsh Anand <utkarshanand4121@gmail.com>
2023-11-10 16:19:34 +0530

    Graph Tutorial

--- DSA Tutorial/Graphs.java
+++ DSA Tutorial/Graphs.java
@@ -463,6 +463,58 @@ public class Graphs {
         }
         System.out.println();
     }
+
+    // Bellman Ford Algorithm
+    static void createGraph6(ArrayList<Edge> graph[]) {
+        for(int i=0; i<graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0, 1, 2));
+        graph[0].add(new Edge(0, 2, 4));
+
+        graph[1].add(new Edge(1, 2, -4));
+
+        graph[2].add(new Edge(2, 3, 2));
+
+        graph[3].add(new Edge(3, 4, 4));
+
+        graph[4].add(new Edge(4, 1, -1));
+    }
+    public static void bellmanFord(ArrayList<Edge> graph[], int src) { // O(V*E)
+        int dist[] = new int[graph.length];
+        for(int i=0; i<dist.length; i++) {
+            if(i != src) {
+                dist[i] = Integer.MAX_VALUE;
+            }
+        }
+
+        int V = graph.length;
+        // algo - O(V)
+        for(int i=0; i<V-1; i++) {
+            // edges - O(E)
+            for(int j=0; j<graph.length; j++) {
+                for(int k=0; k<graph[j].size(); k++) {
+                    Edge e = graph[j].get(k);
+                    // u, v, wt
+                    int u = e.src;
+                    int v = e.dest;
+                    int wt = e.wt;
+                    // relaxation
+                    if(dist[u] != Integer.MAX_VALUE && dist[u] + wt < dist[v]) {
+                        dist[v] = dist[u] + wt;
+                    }
+                }
+            }
+            
+        }
+
+        // print
+        for(int i=0; i<dist.length; i++) {
+            System.out.print(dist[i] + " ");
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
         // int V = 7;
         // ArrayList<Edge> graph[] = new ArrayList[V];
@@ -491,10 +543,12 @@ public class Graphs {
         // int src = 5, dest = 1;
         // printAllPath(graph, src, dest, "");
 
-        int V = 6;
+        int V = 5;
         ArrayList<Edge> graph[] = new ArrayList[V];
         int src = 0;
-        createGraph5(graph);
-        dijkstra(graph, src);
+        createGraph6(graph);
+        //dijkstra(graph, src);
+
+        bellmanFord(graph, src);
     }
 }

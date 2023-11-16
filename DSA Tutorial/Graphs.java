5b29d59499588944701354b41329606512167d51
Utkarsh Anand <utkarshanand4121@gmail.com>
2023-11-16 08:38:08 +0530

    Graph Tutorial

--- DSA Tutorial/Graphs.java
+++ DSA Tutorial/Graphs.java
@@ -687,6 +687,84 @@ public class Graphs {
         return finalCost;
     }
 
+    // Kruskals Algorithm
+    static class Edge4 implements Comparable<Edge4> {
+        int src;
+        int dest;
+        int wt;
+        public Edge4(int s, int d, int w) {
+            this.src = s;
+            this.dest = d;
+            this.wt = w;
+        }
+
+        @Override
+        public int compareTo(Edge4 e2) {
+            return this.wt - e2.wt;
+        }
+    }
+
+    static void createGraphss(ArrayList<Edge4> edges) {
+        // edges
+        edges.add(new Edge4(0, 1, 10));
+        edges.add(new Edge4(0, 2, 15));
+        edges.add(new Edge4(0, 3, 30));
+        edges.add(new Edge4(1, 3, 40));
+        edges.add(new Edge4(2, 3, 50));
+    }
+
+    static int n = 4; //vertices
+    static int par[] = new int[n];
+    static int rank[] = new int[n];
+
+    public static void init() {
+        for(int i=0; i<n; i++) {
+            par[i] = i;
+        }
+    }
+    public static int find(int x) {
+        if(par[x] == x) {
+            return x;
+        }
+
+        return par[x] = find(par[x]);
+    }
+
+    public static void union(int a, int b) {
+        int parA = find(a);
+        int parB = find(b);
+
+        if(rank[parA] == rank[parB]) {
+            par[parB] = parA;
+            rank[parA]++;
+        } else if(rank[parA] < rank[parB]) {
+            par[parA] = parB;
+        } else {
+            par[parB] = parA;
+        }
+    }
+
+    public static void kruskalsMST(ArrayList<Edge4> edges, int V) { // O(V + ElogE)
+        init();
+        Collections.sort(edges); // O(ElogE)
+        int mstCost = 0;
+        int count = 0;
+
+        for(int i=0; count<V-1; i++) { //O(V)
+            Edge4 e = edges.get(i);
+            // (src, dest, wt)
+
+            int parA = find(e.src); //src = a
+            int parB = find(e.dest); //dest = b
+            if(parA != parB) {
+                union(e.src, e.dest);
+                mstCost += e.wt;
+                count++;
+            }
+        }
+
+        System.out.println(mstCost);
+    }
     public static void main(String[] args) {
         // int V = 7;
         // ArrayList<Edge> graph[] = new ArrayList[V];
@@ -733,11 +811,16 @@ public class Graphs {
         // System.out.println(cheapestFlight(n, flights, src, dst, k));
 
         // Connecting cities
-        int cities[][] = { { 0, 1, 2, 3, 4 },
-                { 1, 0, 5, 0, 7 },
-                { 2, 5, 0, 6, 0 },
-                { 3, 0, 6, 0, 0 },
-                { 4, 7, 0, 0, 0 } };
-        System.out.println(connectCities(cities));
+        // int cities[][] = { { 0, 1, 2, 3, 4 },
+        //         { 1, 0, 5, 0, 7 },
+        //         { 2, 5, 0, 6, 0 },
+        //         { 3, 0, 6, 0, 0 },
+        //         { 4, 7, 0, 0, 0 } };
+        // System.out.println(connectCities(cities));
+
+        int V = 4;
+        ArrayList<Edge4> edges = new ArrayList<>();
+        createGraphss(edges);
+        kruskalsMST(edges, V);
     }
 }

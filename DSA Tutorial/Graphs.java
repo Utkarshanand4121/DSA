82295372627133b928272d6066f5fa98af4d8c6f
Utkarsh Anand <utkarshanand4121@gmail.com>
2023-11-16 10:11:14 +0530

    Graph Tutorial

--- DSA Tutorial/Graphs.java
+++ DSA Tutorial/Graphs.java
@@ -765,6 +765,27 @@ public class Graphs {
 
         System.out.println(mstCost);
     }
+
+    // Flood Fill
+    public void helper(int[][] image, int sr, int sc, int color, boolean vis[][], int orgCol){ // O(m*n)
+        if(sr < 0 || sc < 0 || sr >= image.length || sc >= image[0].length || vis[sr][sc] || image[sr][sc] != orgCol) {
+            return;
+        } 
+        image[sr][sc] = color;
+        // left
+        helper(image, sr, sc-1, color, vis, orgCol);
+        // right
+        helper(image, sr, sc+1, color, vis, orgCol);
+        // up
+        helper(image, sr-1, sc, color, vis, orgCol);
+        // down
+        helper(image, sr+1, sc, color, vis, orgCol);
+    }
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        boolean vis[][] = new boolean[image.length][image[0].length];
+        helper(image, sr, sc, color, vis, image[sr][sc]);
+        return image;
+    }
     public static void main(String[] args) {
         // int V = 7;
         // ArrayList<Edge> graph[] = new ArrayList[V];

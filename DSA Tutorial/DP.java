4a55e4383938601fe06bb618f3a47bc6de95a6e5
Utkarsh Anand <utkarshanand4121@gmail.com>
2023-12-01 23:10:00 +0530

    Tutorial

--- DSA Tutorial/DP.java
+++ DSA Tutorial/DP.java
@@ -84,6 +84,27 @@ public class DP {
             return knapsack(val, wt, W, n-1);
         }
     }
+
+    // Memoization Method -- O(n * W)
+    public static int knapsack2(int val[], int wt[], int W, int n, int dp[][]) {
+        if(W == 0 || n == 0) {
+            return 0;
+        }
+        if(dp[n][W] != -1) {
+            return dp[n][W];
+        }
+        if(wt[n-1] <= W) { // valid
+            // include
+            int ans1 = val[n-1] + knapsack2(val, wt, W-wt[n-1], n-1, dp);
+            // exclude
+            int ans2 = knapsack2(val, wt, W, n-1, dp);
+            dp[n][W] = Math.max(ans1, ans2);
+            return dp[n][W];
+        } else { // not valid
+            dp[n][W] = knapsack2(val, wt, W, n-1, dp);
+            return dp[n][W];
+        }
+    }
     public static void main(String[] args) {
         int n = 5;
         int f[] = new int[n + 1]; // 0,0,0,0
@@ -98,6 +119,13 @@ public class DP {
         int val[] = { 15, 14, 10, 45, 30 };
         int wt[] = { 2, 5, 1, 3, 4 };
         int W = 7;
-        System.out.println(knapsack(val, wt, W, val.length));
+        int dp[][] = new int[val.length+1][W+1];
+        for(int i=0; i<dp.length; i++) {
+            for(int j=0; j<dp[0].length; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        System.out.println(knapsack2(val, wt, W, val.length, dp));
+
     }
 }

15d11e69ee5fe44d13e12e085c53e4aea8d3f57a
Utkarsh Anand <utkarshanand4121@gmail.com>
2023-12-31 20:12:13 +0530

    End 2023

--- DSA Tutorial/DP.java
+++ DSA Tutorial/DP.java
@@ -209,6 +209,32 @@ public class DP {
         return dp[n][W];
     }
 
+    // Coin change
+    public static int coinChange(int coins[], int sum) {
+        int n = coins.length;
+        int dp[][] = new int[n+1][sum+1];
+
+        // initialize - sum is 0
+        // i -> coins; j -> sum/change
+        for(int i=0; i<n+1; i++) {
+            dp[i][0] = 1;
+        }
+        
+        for(int j=1; j<sum+1; j++) {
+            dp[0][j] = 0;
+        }
+        // O(N*SUM)
+        for(int i=1; i<n+1; i++) {
+            for(int j=1; j<sum+1; j++) { //valid
+                if(coins[i-1] <= j) {
+                    dp[i][j] = dp[i][j-coins[i-1]] + dp[i-1][j];
+                } else { // invalid
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][sum];
+    }
     public static void main(String[] args) {
         // int n = 5;
         // int f[] = new int[n + 1]; // 0,0,0,0
@@ -238,9 +264,14 @@ public class DP {
         // System.out.println(targetSumSubset(arr, sum));
 
         // Unbounded Knapsack
-        int val[] = { 15, 14, 10, 45, 30 };
-        int wt[] = { 2, 5, 1, 3, 4 };
-        int W = 7;
-        System.out.println(unboundedKnapsack(val, wt, W));
+        // int val[] = { 15, 14, 10, 45, 30 };
+        // int wt[] = { 2, 5, 1, 3, 4 };
+        // int W = 7;
+        // System.out.println(unboundedKnapsack(val, wt, W));
+
+        // Coin change
+        int coins[] = {2,5,3,6};
+        int sum = 10; // ans = 4
+        System.out.println(coinChange(coins, sum));
     }
 }

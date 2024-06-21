e2ad74a99e463fb487eaee1f123d04a2938d83b5
Utkarsh Anand <utkarshanand4121@gmail.com>
2024-06-21 17:36:25 +0530

    Commit after a long time

--- DSA Tutorial/DP.java
+++ DSA Tutorial/DP.java
@@ -239,29 +239,95 @@ public class DP {
     // Rod Cutting
     public static int rodCutting(int length[], int price[], int totRod) {
         int n = price.length;
-        int dp[][] = new int[n+1][totRod+1];
+        int dp[][] = new int[n + 1][totRod + 1];
 
-        for(int i=0; i<n+1; i++) {
-            for(int j=0; j<totRod+1; j++) {
-                if(i == 0 || j == 0) {
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < totRod + 1; j++) {
+                if (i == 0 || j == 0) {
                     dp[i][j] = 0;
                 }
             }
         }
 
-        for(int i=1; i<n+1; i++) {
-            for(int j=1; j<totRod+1; j++) {
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < totRod + 1; j++) {
                 // valid
-                if(length[i-1] <= j) {
+                if (length[i - 1] <= j) {
                     // dp[i][j] = Math.max(val[i-1] + dp[i][j-wt[i-1]], dp[i-1][j]);
-                    dp[i][j] = Math.max(price[i-1] + dp[i][j-length[i-1]], dp[i-1][j]);
+                    dp[i][j] = Math.max(price[i - 1] + dp[i][j - length[i - 1]], dp[i - 1][j]);
                 } else { // invlaid
-                    dp[i][j] = dp[i-1][j];
+                    dp[i][j] = dp[i - 1][j];
                 }
             }
         }
         return dp[n][totRod];
-    } 
+    }
+
+    // Longest Common Subsequence
+    public static int lcs(String str1, String str2, int n, int m) {
+        if (n == 0 || m == 0) {
+            return 0;
+        }
+
+        if (str1.charAt(n - 1) == str2.charAt(m - 1)) { // same
+            return lcs(str1, str2, n - 1, m - 1) + 1;
+        } else { // diff
+            int ans1 = lcs(str1, str2, n - 1, m);
+            int ans2 = lcs(str1, str2, n, m - 1);
+            return Math.max(ans1, ans2);
+        }
+    }
+
+    // LCS ---- Memoization Code
+    public static int lcsMemo(String str1, String str2, int n, int m, int dp[][]) {
+        if (n == 0 || m == 0) {
+            return 0;
+        }
+
+        if (dp[n][m] != -1) {
+            return dp[n][m];
+        }
+
+        if (str1.charAt(n - 1) == str2.charAt(m - 1)) { // same
+            return dp[n][m] = lcsMemo(str1, str2, n - 1, m - 1, dp) + 1;
+        } else { // diff
+            int ans1 = lcsMemo(str1, str2, n - 1, m, dp);
+            int ans2 = lcsMemo(str1, str2, n, m - 1, dp);
+            return dp[n][m] = Math.max(ans1, ans2);
+        }
+    }
+
+    // LCS ---- Tabulation
+    // O(n * m)
+    public static int lcsTab(String str1, String str2) {
+        int n = str1.length();
+        int m = str2.length();
+
+        int dp[][] = new int[n + 1][m + 1];
+        // initialization
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < m + 1; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    int ans1 = dp[i - 1][j];
+                    int ans2 = dp[i][j - 1];
+                    dp[i][j] = Math.max(ans1, ans2);
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+
     public static void main(String[] args) {
         // int n = 5;
         // int f[] = new int[n + 1]; // 0,0,0,0
@@ -302,9 +368,31 @@ public class DP {
         // System.out.println(coinChange(coins, sum));
 
         // Rod Cutting
-        int length[] = {1,2,3,4,5,6,7,8};
-        int price[] = {1,5,8,9,10,17,17,20};
-        int totRod = 8;
-        System.out.println(rodCutting(length, price, totRod));
+        // int length[] = {1,2,3,4,5,6,7,8};
+        // int price[] = {1,5,8,9,10,17,17,20};
+        // int totRod = 8;
+        // System.out.println(rodCutting(length, price, totRod));
+
+        // LCS
+        // String str1 = "abcdge";
+        // String str2 = "abedg"; // lcs = "abdg"; length = 4
+        // System.out.println(lcs(str1, str2, str1.length(), str2.length()));
+
+        // LCS Memoization
+        // int n = str1.length();
+        // int m = str2.length();
+        // int dp[][] = new int[n + 1][m + 1];
+        // initialization
+        // for (int i = 0; i < n + 1; i++) {
+        //     for (int j = 0; j < m + 1; j++) {
+        //         dp[i][j] = -1;
+        //     }
+        // }
+        // System.out.println(lcsMemo(str1, str2, n, m, dp));
+
+        // LCS Tabulation
+        String str1 = "abcde";
+        String str2 = "ace";
+        System.out.println(lcsTab(str1, str2));
     }
 }

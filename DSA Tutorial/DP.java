f5f05f93aef670d3e963f0fb9a5bceaf12adbdc9
Utkarsh Anand <utkarshanand4121@gmail.com>
2024-06-26 11:59:45 +0530

    DP

--- DSA Tutorial/DP.java
+++ DSA Tutorial/DP.java
@@ -538,6 +538,44 @@ public class DP {
         return dp[n];
     }
 
+    // Count BST
+    public static int countBST(int n) {
+        // O(n^2)
+        int dp[] = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i < n + 1; i++) {
+            // Ci -> BST (i nodes) -> dp[i]
+            for (int j = 0; j < i; j++) {
+                int left = dp[j];
+                int right = dp[i - j - 1];
+                dp[i] += left * right;
+            }
+        }
+
+        return dp[n];
+    }
+
+    // Mountains Ranges
+    public static int mountainRanges(int n) {
+        // O(n^2)
+        int dp[] = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i < n + 1; i++) {
+            // i pairs -> mountain ranges => Ci
+            for (int j = 0; j < i; j++) {
+                int inside = dp[j];
+                int outside = dp[i - j - 1];
+                dp[i] += inside * outside;
+            }
+        }
+
+        return dp[n];
+    }
+
     public static void main(String[] args) {
         // int n = 5;
         // int f[] = new int[n + 1]; // 0,0,0,0
@@ -633,11 +671,19 @@ public class DP {
         // int n = 5;
         // System.out.println(catalanRec(n));
         // Memoization
-        int n = 4;
-        int dp[] = new int[n + 1];
-        Arrays.fill(dp, -1);
-        System.out.println(catalanMemo(n, dp));
+        // int n = 4;
+        // int dp[] = new int[n + 1];
+        // Arrays.fill(dp, -1);
+        // System.out.println(catalanMemo(n, dp));
         // Tabulation
-        System.out.println(catalanTab(4));
+        // System.out.println(catalanTab(4));
+
+        // Count BST
+        // int n = 4;
+        // System.out.println(countBST(n));
+
+        // Mountains ranges
+        int n = 4;
+        System.out.println(mountainRanges(n));
     }
 }

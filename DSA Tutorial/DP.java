f23b51777058768a8ecf70c7adc97ecdc3f16d68
Utkarsh Anand <utkarshanand4121@gmail.com>
2024-06-27 23:42:06 +0530

    DP

--- DSA Tutorial/DP.java
+++ DSA Tutorial/DP.java
@@ -576,6 +576,26 @@ public class DP {
         return dp[n];
     }
 
+    // MCM
+    // Recursion
+    public static int mcm(int arr[], int i, int j) {
+        if (i == j) {
+            return 0;
+        }
+
+        int ans = Integer.MAX_VALUE;
+        for (int k = i; k <= j - 1; k++) {
+            int cost1 = mcm(arr, i, k); // Ai...Ak => arr[i-1] x arr[k]
+            int cost2 = mcm(arr, k + 1, j); // Ai+1...Aj => arr[k] x arr[j]
+            int cost3 = arr[i - 1] * arr[k] * arr[j];
+            int finalCost = cost1 + cost2 + cost3;
+
+            ans = Math.min(ans, finalCost);
+        }
+
+        return ans; // minCost
+    }
+
     public static void main(String[] args) {
         // int n = 5;
         // int f[] = new int[n + 1]; // 0,0,0,0
@@ -683,7 +703,13 @@ public class DP {
         // System.out.println(countBST(n));
 
         // Mountains ranges
-        int n = 4;
-        System.out.println(mountainRanges(n));
+        // int n = 4;
+        // System.out.println(mountainRanges(n));
+
+        // MCM
+        // Recursion
+        int arr[] = { 1, 2, 3, 4, 3 }; // n=5
+        int n = arr.length;
+        System.out.println(mcm(arr, 1, n - 1));
     }
 }

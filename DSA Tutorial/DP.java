69db88392a2c108e00dd8861642a7c4e960217ec
Utkarsh Anand <utkarshanand4121@gmail.com>
2024-06-28 15:38:24 +0530

    Nice

--- DSA Tutorial/DP.java
+++ DSA Tutorial/DP.java
@@ -596,6 +596,56 @@ public class DP {
         return ans; // minCost
     }
 
+    // Memoization
+    //O(n^2)
+    public static int mcmMem(int arr[], int i, int j, int dp[][]) {
+        if (i == j) {
+            return 0;
+        }
+
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+
+        int ans = Integer.MAX_VALUE;
+        for (int k = i; k <= j - 1; k++) {
+            int cost1 = mcmMem(arr, i, k, dp);
+            int cost2 = mcmMem(arr, k + 1, j, dp);
+            int cost3 = arr[i - 1] * arr[k] * arr[j];
+            int finalCost = cost1 + cost2 + cost3;
+            ans = Math.min(ans, finalCost);
+            dp[i][j] = ans;
+        }
+
+        return ans;
+    }
+
+    // Tabulation
+    public static int mcmTab(int arr[]) {
+        int n = arr.length;
+        int dp[][] = new int[n][n];
+
+        //initialization
+        for(int i=0; i<n; i++) {
+            dp[i][i] = 0;
+        }
+
+        //bottom-up
+        for(int len=2; len<=n-1; len++) {
+            for(int i=1; i<=n-len; i++) {
+                int j = i+len-1; //col
+                dp[i][j] = Integer.MAX_VALUE;
+                for(int k=i; k<=j-1; k++) {
+                    int cost1 = dp[i][k];
+                    int cost2 = dp[k+1][j];
+                    int cost3 = arr[i-1] * arr[k] * arr[j];
+                    dp[i][j] = Math.min(dp[i][j], cost1 + cost2 + cost3);
+                }
+            }
+        }
+        print(dp);
+        return dp[1][n-1];
+    }
     public static void main(String[] args) {
         // int n = 5;
         // int f[] = new int[n + 1]; // 0,0,0,0
@@ -690,11 +740,13 @@ public class DP {
         // Catalan's Number
         // int n = 5;
         // System.out.println(catalanRec(n));
+
         // Memoization
         // int n = 4;
         // int dp[] = new int[n + 1];
         // Arrays.fill(dp, -1);
         // System.out.println(catalanMemo(n, dp));
+
         // Tabulation
         // System.out.println(catalanTab(4));
 
@@ -708,8 +760,21 @@ public class DP {
 
         // MCM
         // Recursion
+        // int arr[] = { 1, 2, 3, 4, 3 }; // n=5
+        // int n = arr.length;
+        // System.out.println(mcm(arr, 1, n - 1));
+
+        // Memoization
+        // int arr[] = { 1, 2, 3, 4, 3 }; // n=5
+        // int n = arr.length;
+        // int dp[][] = new int[n][n];
+        // for (int i = 0; i < n; i++) {
+        //     Arrays.fill(dp[i], -1);
+        // }
+        // System.out.println(mcmMem(arr, 1, n - 1, dp));
+
+        // Tabulation
         int arr[] = { 1, 2, 3, 4, 3 }; // n=5
-        int n = arr.length;
-        System.out.println(mcm(arr, 1, n - 1));
+        System.out.println(mcmTab(arr));
     }
 }

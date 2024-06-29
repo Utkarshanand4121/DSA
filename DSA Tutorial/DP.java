7c1f95cdcad21557d4e493cccfc73a429eee8b1a
Utkarsh Anand <utkarshanand4121@gmail.com>
2024-06-29 16:22:23 +0530

    The END of DP

--- DSA Tutorial/DP.java
+++ DSA Tutorial/DP.java
@@ -597,7 +597,7 @@ public class DP {
     }
 
     // Memoization
-    //O(n^2)
+    // O(n^2)
     public static int mcmMem(int arr[], int i, int j, int dp[][]) {
         if (i == j) {
             return 0;
@@ -625,27 +625,77 @@ public class DP {
         int n = arr.length;
         int dp[][] = new int[n][n];
 
-        //initialization
-        for(int i=0; i<n; i++) {
+        // initialization
+        for (int i = 0; i < n; i++) {
             dp[i][i] = 0;
         }
 
-        //bottom-up
-        for(int len=2; len<=n-1; len++) {
-            for(int i=1; i<=n-len; i++) {
-                int j = i+len-1; //col
+        // bottom-up
+        for (int len = 2; len <= n - 1; len++) {
+            for (int i = 1; i <= n - len; i++) {
+                int j = i + len - 1; // col
                 dp[i][j] = Integer.MAX_VALUE;
-                for(int k=i; k<=j-1; k++) {
+                for (int k = i; k <= j - 1; k++) {
                     int cost1 = dp[i][k];
-                    int cost2 = dp[k+1][j];
-                    int cost3 = arr[i-1] * arr[k] * arr[j];
+                    int cost2 = dp[k + 1][j];
+                    int cost3 = arr[i - 1] * arr[k] * arr[j];
                     dp[i][j] = Math.min(dp[i][j], cost1 + cost2 + cost3);
                 }
             }
         }
         print(dp);
-        return dp[1][n-1];
+        return dp[1][n - 1];
     }
+
+    // Minimum Partition
+    public static int minPartition(int arr[]) { // O(n*W)
+        int n = arr.length;
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+        }
+
+        int W = sum / 2;
+        int dp[][] = new int[n + 1][W + 1];
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < W + 1; j++) {
+                if (arr[i - 1] <= j) { // valid
+                    dp[i][j] = Math.max(arr[i - 1] + dp[i - 1][j - arr[i - 1]], dp[i - 1][j]);
+                } else { // invalid
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        int sum1 = dp[n][W];
+        int sum2 = sum - sum1;
+        return Math.abs(sum1 - sum2);
+    }
+
+    // Min Jumps Array
+    public static int minJumps(int nums[]) {
+        int n = nums.length;
+        int dp[] = new int[n];
+        Arrays.fill(dp, -1);
+        dp[n - 1] = 0;
+
+        for (int i = n - 2; i >= 0; i--) {
+            int steps = nums[i];
+            int ans = Integer.MAX_VALUE;
+            for (int j = i + 1; j <= i + steps && j < n; j++) {
+                if (dp[j] != -1) {
+                    ans = Math.min(ans, dp[j] + 1);
+                }
+            }
+            if (ans != Integer.MAX_VALUE) {
+                dp[i] = ans;
+            }
+        }
+
+        return dp[0]; // 0 -> n-1
+    }
+
     public static void main(String[] args) {
         // int n = 5;
         // int f[] = new int[n + 1]; // 0,0,0,0
@@ -769,12 +819,20 @@ public class DP {
         // int n = arr.length;
         // int dp[][] = new int[n][n];
         // for (int i = 0; i < n; i++) {
-        //     Arrays.fill(dp[i], -1);
+        // Arrays.fill(dp[i], -1);
         // }
         // System.out.println(mcmMem(arr, 1, n - 1, dp));
 
         // Tabulation
-        int arr[] = { 1, 2, 3, 4, 3 }; // n=5
-        System.out.println(mcmTab(arr));
+        // int arr[] = { 1, 2, 3, 4, 3 }; // n=5
+        // System.out.println(mcmTab(arr));
+
+        // Minimum Partition
+        // int arr[] = {1,6,11,5};
+        // System.out.println(minPartition(arr));
+
+        // Min Jumps Array
+        int nums[] = { 2, 3, 1, 1, 4 };
+        System.out.println(minJumps(nums));
     }
 }

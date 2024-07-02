d160600329f5acef268e5ad01b8ac57602e9c9b6
Utkarsh Anand <utkarshanand4121@gmail.com>
2024-07-02 11:54:39 +0530

    DSA Completed

--- DSA Tutorial/ST.java
+++ DSA Tutorial/ST.java
@@ -59,21 +59,90 @@ public class ST {
         updateUtil(0, 0, n - 1, idx, diff);
     }
 
-    public static void main(String[] args) {
-        int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
-        int n = arr.length;
-        init(n);
-        buildST(arr, 0, 0, n - 1);
-
-        for (int i = 0; i < tree.length; i++) {
-            System.out.print(tree[i] + " ");
+    // Max/Min Segment Tree
+    public static void constructST(int i, int si, int sj, int arr[]) {
+        if (si == sj) {
+            tree[i] = arr[si];
+            return;
         }
-        System.out.println();
+
+        int mid = (si + sj) / 2;
+        constructST(2 * i + 1, si, mid, arr);
+        constructST(2 * i + 2, mid + 1, sj, arr);
+
+        tree[i] = Math.max(tree[2 * i + 1], tree[2 * i + 2]);
+    }
+
+    // Query
+    public static int getMax(int arr[], int qi, int qj) {
+        int n = arr.length;
+        return getMaxUtil(0, 0, n - 1, qi, qj);
+    }
+
+    public static int getMaxUtil(int i, int si, int sj, int qi, int qj) {
+        if (si > qj || sj < qi) { // no overlap
+            return Integer.MIN_VALUE;
+        } else if (si >= qi && sj <= qj) { // complete overlap
+            return tree[i];
+        } else { // partial overlap
+            int mid = (si + sj) / 2;
+            int leftAns = getMaxUtil(2 * i + 1, si, mid, qi, qj);
+            int rightAns = getMaxUtil(2 * i + 2, mid + 1, sj, qi, qj);
+            return Math.max(leftAns, rightAns);
+        }
+    }
+
+    // Update
+    public static void update2(int arr[], int idx, int newVal) {
+        arr[idx] = newVal;
+        int n = arr.length;
+        updateUtil2(0, 0, n - 1, idx, newVal);
+    }
+
+    public static void updateUtil2(int i, int si, int sj, int idx, int newVal) {
+        if (idx < si || idx > sj) {
+            return;
+        }
+
+        tree[i] = Math.max(tree[i], newVal);
+        if (si != sj) {
+            int mid = (si + sj) / 2;
+            updateUtil2(2 * i + 1, 0, mid, idx, newVal); // left
+            updateUtil2(2 * i + 2, mid + 1, sj, idx, newVal); // right
+        }
+    }
+
+    public static void main(String[] args) {
+        // int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+        // int n = arr.length;
+        // init(n);
+        // buildST(arr, 0, 0, n - 1);
+
+        // for (int i = 0; i < tree.length; i++) {
+        // System.out.print(tree[i] + " ");
+        // }
+        // System.out.println();
         // Query on ST
-        System.out.println(getSum(arr, 2, 5));
+        // System.out.println(getSum(arr, 2, 5));
 
         // Update on ST
-        update(arr, 2, 2);
-        System.out.println(getSum(arr, 2, 5));
+        // update(arr, 2, 2);
+        // System.out.println(getSum(arr, 2, 5));
+
+        // Max/Min Segment Tree
+        int arr1[] = { 6, 8, -1, 2, 17, 1, 3, 2, 4 };
+        int n = arr1.length;
+        init(n);
+        constructST(0, 0, n - 1, arr1);
+        // for(int i=0; i<tree.length; i++) {
+        // System.out.print(tree[i] + " ");
+        // }
+
+        int max = getMax(arr1, 2, 5);
+        System.out.println(max);
+
+        update2(arr1, 2, 20);
+        int max2 = getMax(arr1, 2, 5);
+        System.out.println(max2);
     }
 }
